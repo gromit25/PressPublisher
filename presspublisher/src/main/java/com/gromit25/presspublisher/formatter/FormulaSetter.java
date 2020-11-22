9d1dce9764a202c1359c506cade73e1d63aef00f
gromit25 <gromit25@hanmail.net>
2020-11-22 14:57:12 +0900

    주석 추가

--- presspublisher/src/main/java/com/gromit25/presspublisher/formatter/FormulaSetter.java
+++ presspublisher/src/main/java/com/gromit25/presspublisher/formatter/FormulaSetter.java
@@ -13,7 +13,7 @@ import lombok.Setter;
  * 
  * @author jmsohn
  */
-class FormulaSetter {
+/*package*/ class FormulaSetter {
 	
 	@Getter(AccessLevel.PACKAGE)
 	@Setter(AccessLevel.PACKAGE)
@@ -27,17 +27,17 @@ class FormulaSetter {
 	@Setter(AccessLevel.PACKAGE)
 	private Method writer;
 	
-	FormulaSetter(Evaluator formula, Method setter, Method writer) {
+	/*package*/ FormulaSetter(Evaluator formula, Method setter, Method writer) {
 		this.setFormula(formula);
 		this.setSetter(setter);
 		this.setWriter(writer);
 	}
 	
-	FormulaSetter(String formulaScript, Method setter, Method writer) throws Exception {
+	/*package*/ FormulaSetter(String formulaScript, Method setter, Method writer) throws Exception {
 		this(Evaluator.compile(formulaScript), setter, writer);
 	}
 	
-	void setData(Formatter formatter, ValueContainer values) throws Exception {
+	/*package*/ void setData(Formatter formatter, ValueContainer values) throws Exception {
 		Object retValue = this.getFormula().eval(values, Object.class);
 		this.getSetter().invoke(null, formatter, this.getWriter(), retValue.toString());
 	}
